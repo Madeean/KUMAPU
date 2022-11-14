2f07f326e2d930eef12a0e54e1c9e713e1a572b5
MadeReihan <reihanmade@gmail.com>
2022-11-14 08:37:04 +0700

    pengontrak profile

--- src/pengontrak/PengontrakProfile.tsx
+++ src/pengontrak/PengontrakProfile.tsx
@@ -1,10 +1,60 @@
+import {
+  IonButton,
+  IonButtons,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonRow,
+  IonText,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 import React from "react";
+import "./PengontrakProfile.css";
+import ProfilePage from "../image/ProfilePage.png";
+import { pencil } from "ionicons/icons";
 
 const PengontrakProfile: React.FC = () => {
   return (
-    <div>
-      <h1>Ini Pengontrak Profile</h1>
-    </div>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color="medium">
+          <IonTitle className="ion-text-center profile-text-pengontrak-profile">
+            My Profile
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <img src={ProfilePage} className="image-pengontrak-profile" />
+            </IonCol>
+            <IonCol>
+              <IonTitle>Made Reihan</IonTitle>
+              <IonTitle>+629243423</IonTitle>
+              <IonTitle>Made@gmail.com</IonTitle>
+            </IonCol>
+            <IonCol size="auto">
+              <IonButtons>
+                <IonButton className="btn-edit-pengontrak-profile">
+                  <IonIcon
+                    src={pencil}
+                    className="ion-icon-pengontrak-profile"
+                  />
+                </IonButton>
+              </IonButtons>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+        <div className="btn-logout-pengontrak-profile">
+          <IonButton>Logout</IonButton>
+        </div>
+      </IonContent>
+    </IonPage>
   );
 };
 export default PengontrakProfile;
